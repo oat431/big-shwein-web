218c973fb7421bded76967efd929155c0f3a3183
oat431 <oat431@gmail.com>
2026-03-03 22:46:33 +0700

    init: lodger project for frontend

---
+++ README.md
@@ -0,0 +1,3 @@
+# Big Shwein Web
+
+A lodger application (frontend part)
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+        <p className={"text-green-500 text-xl font-bold"}>Big Shwein Web</p>
+    </>
+  )
+}
+
+export default App
